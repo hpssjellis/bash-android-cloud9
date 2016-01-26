e6ed2d339b30f16fdd89cd37d9ac6854f5d5d91d
Jeremy Ellis <keyfreemusic@gmail.com>
2016-01-26 07:16:28 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -116,9 +116,13 @@ android update sdk --filter extra --no-ui --force
 
 android update sdk --filter extra --no-ui --force
 android update sdk --all --filter build-tools-23.0.2 --no-ui --force
-android update sdk --filter android-20 --no-ui --force   #Android 4.4W.2
-android update sdk --filter android-22 --no-ui --force   #Android 5.1.1
-android update sdk --filter android-23 --no-ui --force   #Android 6.0
+android update sdk --filter android-20 --no-ui --force   #Android 4.4W.2 KitKat
+android update sdk --filter android-22 --no-ui --force   #Android 5.1.1  Lollipop
+android update sdk --filter android-23 --no-ui --force   #Android 6.0    Marshmallow
+
+
+#http://socialcompare.com/en/comparison/android-versions-comparison
+
 
 
 
