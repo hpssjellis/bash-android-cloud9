f2400e619a65bdc1cebe8f48f593b0cbdf7b45e1
Jeremy Ellis <keyfreemusic@gmail.com>
2016-01-26 15:37:00 -0800

    Update a01-create-app-rocksetta.sh

--- a01-create-app-rocksetta.sh
+++ a01-create-app-rocksetta.sh
@@ -1,43 +1,28 @@
 #!/bin/bash  
 
 #  only need to run this script with the command (do not type the #)
-#  bash a01-create-app-rocksetta.sh
+#  bash a06-rocksetta-setup-android.sh
 # best in cloud 9 to just right click this file and select run
 
 
+#change hello-world to the name of your app and folder
+
+echo "Enter a folder name, no spaces, no dashes, underscore allowed, for your app and folder example: helloWorld"
+read wow4
 
 
-#echo "Type the number of the target your Android app is for"
-#echo "for --target android-20    Android 4.4W.2 KitKat, type 20"
-#echo "for --target android-22  Android 5.1.1  Lollipop, type 22"
-#echo "for --target android-23   Android 6.0    Marshmallow, type 23"
+ls -l /home/ubuntu/workspace/android-sdk-linux/platforms
 
-echo "Finding code numbers for the Android SDK platform you wish to install"
-echo "PS. You could type the entire name example android-23"
-echo "Note: This setup comes installed with android-20  Android-22 Android-23"
 
-android list sdk 
 
-echo "------------------------------------------------------------------------"
-echo ""
-echo "Type the left hand side number of the SDK to install. 0 to exit"
+echo "Type the number of the target your Android app is for"
+echo "See above folders of installed platforms"
+echo "for --target android-20    Android 4.4W.2 KitKat, type 20"
+echo "for --target android-22    Android 5.1.1  Lollipop, type 22"
+echo "for --target android-23    Android 6.0    Marshmallow, type 23"
 
 read myNum
 
-
-if [ $myNum == 0 ]; then
-  echo "Exiting without doing anything."
-  exit
-
-  else
-     echo "Installing choice number $myNum"
- 
-
-
-
-
-
-android update sdk --filter $myNum --no-ui --force 
 
 
 # the commands to install the platforms
@@ -50,6 +35,38 @@ android update sdk --filter $myNum --no-ui --force
 #http://socialcompare.com/en/comparison/android-versions-comparison
 
 
+android create project \
+--target android-$myNum \
+--name $wow4 \
+--path $wow4 \
+--activity $wow4 \
+--package com.example.$wow4
 
 
-  fi
+
+
+
+#android update project --path .
+
+#ant
+
+android update project --name $wow4 --target android-$myNum --path $wow4
+
+cd $wow4
+
+ant clean
+
+ant debug
+
+cd bin
+
+
+printf "\n\n<a href='$wow4/bin/$wow4-debug.apk'>$wow4/bin/$wow4-debug.apk</a><br>"  >> /home/ubuntu/workspace/index.html
+
+
+
+ls -l
+
+
+echo "Look for you new android $wow4/bin $wow4-debug.apk"
+echo "rightclick run index.html, then preview-preview running application to view webpage with .apk"
