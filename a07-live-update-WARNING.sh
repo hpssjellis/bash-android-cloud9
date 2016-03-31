f1a58b874a2c9effb582d9f9caea1ddb3b40e5bd
Jeremy Ellis <keyfreemusic@gmail.com>
2016-03-31 15:08:27 -0700

    Update a07-live-update-WARNING.sh

--- a07-live-update-WARNING.sh
+++ a07-live-update-WARNING.sh
@@ -8,3 +8,6 @@
 
 echo "WARNING THIS MAY CHANGE YOUR WORKSPACE, IF UNSURE EXIT THIS TERMINAL"
 echo "This bash file will update the workspace from the github repository at "
+echo "https://github.com/hpssjellis/online-android-sdk-cloud9-live-update"
+
+sudo git clone https://github.com/hpssjellis/online-android-sdk-cloud9-live-update /home/ubuntu
